5326da7bf5ef7e8bf2b25931e589be7037b4dc0b
Roman-Sykhyi <kariys2015@gmail.com>
2021-10-06 20:12:16 +0300

    Виконано задачу 3

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -6,7 +6,27 @@ namespace Задача_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string filePath = @"c:\WebServers\home\testsite\www\myfile.txt";
+
+            string fileName = FindFileName(filePath);
+            string rootDirectory = FindRootDirectory(filePath);
+
+            Console.WriteLine("Назва файлу без розширення: " + fileName);
+            Console.WriteLine("Коренева папка: " + rootDirectory);
+        }
+
+        private static string FindFileName(string filePath)
+        {
+            int lastSlashPos = filePath.LastIndexOf('\\');
+            int dotPos = filePath.LastIndexOf('.');
+
+            return filePath.Substring(lastSlashPos + 1, dotPos - lastSlashPos - 1);
+        }
+
+        private static string FindRootDirectory(string filePath)
+        {
+            string[] parts = filePath.Split('\\');
+            return parts[1];
         }
     }
 }
